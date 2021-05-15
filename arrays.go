409f8a494925b9ff0b7117bc1a88919ade32d183
TiagoCiceri <tiagociceri@gmail.com>
2021-05-15 15:40:57 -0300

    execicio de array com for

--- arrays.go
+++ arrays.go
@@ -2,12 +2,22 @@ package main
 
 import "fmt"
 
-func main(){	
-	var numeros [7] int
+func main(){		
+	var numeros [17] int
 	
-	for i := 0; i < 7; i++ {
+	for i := 0; i < len(numeros); i++ {
 		numeros[i] = i
-	}
-	
+	}	
 	fmt.Println(numeros)	
+
+	var novoarray = [5] int {10, 20, 30, 4, 5}	
+	fmt.Println(novoarray)
+
+	var soma int =0
+
+	for i := 0; i < len(novoarray); i++ {
+		soma += novoarray[i]
+	}
+	fmt.Println("Soma do array: ", soma)
+
 }
