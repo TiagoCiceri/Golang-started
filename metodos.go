151de7b9939dd97b04ddaf0c8e058473cdaec713
TiagoCiceri <tiagociceri@gmail.com>
2021-05-22 09:17:50 -0300

    aprofundando em metodos

--- metodos.go
+++ metodos.go
@@ -11,9 +11,14 @@ func (str Pessoa) nomeCompleto() string {
 	return str.Nome +" "+ str.Sobrenome
 }
 
+func nfNomeCompleto(str Pessoa) string{
+	return str.Nome+" "+str.Sobrenome
+}
+
 func main(){
 	alguem := Pessoa{"João", "de Alencar"}
 
 	fmt.Println(alguem)
-	fmt.Println("Nome completo: ",alguem.nomeCompleto())
+	fmt.Println("Nome completo: ", alguem.nomeCompleto())
+	fmt.Println("Nome completo: ", nfNomeCompleto(alguem))
 }
